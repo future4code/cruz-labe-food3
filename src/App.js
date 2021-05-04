8634823e94160bec057bc1175b18b4bdb79d7189
Paula Lopes <linha_vitoria@hotmail.com>
2021-05-04 15:07:18 -0300

    alteracoes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import theme from "./constants/theme";
 import LoginPage from "./pages/loginPage/LoginPage";
 import SignUpPage from "./pages/signUpPage/SignUpPage";
 import AddAddressPage from "./pages/addAddressPage/AddAddressPage";
-import ProfilePage from "./pages/profilePage/ProfilePage";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <ProfilePage />
+      <AddAddressPage />
     </ThemeProvider>
   );
 };
