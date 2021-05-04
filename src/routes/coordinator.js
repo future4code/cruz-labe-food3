bb3211b7c83a9b65f19b8a296f8848d54897bb1d
Fagner Zulin <fagnerzulin11@gmail.com>
2021-05-04 15:07:52 -0300

    Stating to do coordinators

---
+++ src/routes/coordinator.js
@@ -0,0 +1,23 @@
+export const gotToHomePage = (history) => {
+  history.push("/");
+};
+
+export const gotToSignUpPage = (history) => {
+  history.push("/signup");
+};
+
+export const gotToLoginPage = (history) => {
+  history.push("/login");
+};
+
+export const gotToAddAddressPage = (history) => {
+  history.push("/addAddress");
+};
+
+export const gotToMyCartPage = (history) => {
+  history.push("/cart");
+};
+
+export const gotToProfile = (history) => {
+  history.push("/profile");
+};
