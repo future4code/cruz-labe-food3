90088105179944833a541fed4c21cc13adb9c2d2
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-05-04 15:06:16 -0300

    Layout da tag do pedido em andamento finalizada.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import theme from "./constants/theme";
 import LoginPage from "./pages/loginPage/LoginPage";
 import SignUpPage from "./pages/signUpPage/SignUpPage";
 import AddAddressPage from "./pages/addAddressPage/AddAddressPage";
+import OrderInProgress from "./components/orderInProgress/OrderInProgress";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <SignUpPage />
+      <OrderInProgress />
     </ThemeProvider>
   );
 };
