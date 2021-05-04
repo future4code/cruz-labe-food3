ae69f3ecc124020936b460e054008d646021b630
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-05-04 16:04:16 -0300

    Tag do pedido em andamento corrigida.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "@material-ui/core";
 import theme from "./constants/theme";
 import AddAddressPage from "./pages/addAddressPage/AddAddressPage";
 import OrderInProgress from "./components/orderInProgress/OrderInProgress";
-import Restaurants from "./pages/restaurants/restaurants"
+import Restaurants from "./pages/restaurants/restaurants";
 import "./assets/css/fonts.css";
 import Router from "./routes/Router";
 
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router />
+      <OrderInProgress />
     </ThemeProvider>
   );
 };
