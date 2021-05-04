772e2e0b580bad1efc0a9dc2953656edacbfdaf1
Paula Lopes <linha_vitoria@hotmail.com>
2021-05-04 14:59:34 -0300

    criação da pagina de perfil

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import theme from "./constants/theme";
 import LoginPage from "./pages/loginPage/LoginPage";
 import SignUpPage from "./pages/signUpPage/SignUpPage";
 import AddAddressPage from "./pages/addAddressPage/AddAddressPage";
+import ProfilePage from "./pages/profilePage/ProfilePage";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <AddAddressPage />
+      <ProfilePage />
     </ThemeProvider>
   );
 };
